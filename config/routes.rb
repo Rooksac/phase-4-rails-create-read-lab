8bd5672d195170538bfb65b1711d40765847e04d
root <root@DESKTOP-NH2VI3D.localdomain>
2022-09-19 16:31:50 -0400

    commit tree 92c6c929260d08310b7eb6d1236288e80eb7ac08

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'plants/:id', to: 'plants#show'
+  get '/plants', to: 'plants#index'
+  post '/plants', to: 'plants#create'
 end
